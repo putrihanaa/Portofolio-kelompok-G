6e28668bc63fe84605c121383ac4b286433f70bc
unknown <putrihana65@gmail.com>
2023-12-13 22:38:05 +0700

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,73 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "LOGO",
+      description: "Design Logo",
+      imgUrl: projImg1,
+    },
+    {
+      title: "PAMFLET",
+      description: "Design Pamflet",
+      imgUrl: projImg2,
+    },
+    {
+      title: "E-COMMERCE",
+      description: "Design E-commerce",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Contoh Project yang pernah dibuat Kelompok G</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  
+                    <Nav.Item>
+                      <Nav.Link eventKey="first"></Nav.Link>
+                    </Nav.Item>
+                    
+              
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
